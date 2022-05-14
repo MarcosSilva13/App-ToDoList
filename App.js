7a35d6c8c17112a935eb7f5cff359631a09db743
MarcosSilva13 <marcosslp45@gmail.com>
2022-05-14 11:57:43 -0300

    Criando campo input e botão

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+import {Ionicons, MaterialIcons} from "@expo/vector-icons"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <View style={styles.Body}>
+          <Text>Hello World!</Text>
+        </View>
+        <View style={styles.Form}>
+          <TextInput 
+            style={styles.Input} 
+            placeholderTextColor="#999"
+            autoCorrect={true}
+            placeholder="Adicione uma tarefa"
+            maxLength={25}
+          />
+          <TouchableOpacity style={styles.Button}>
+            <Ionicons name="ios-add" size={25} color="#fff"/>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    marginTop: 20
   },
+  Body: {
+    flex: 1,
+  },
+  Form: {
+    padding: 0,
+    height: 60,
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    paddingTop: 13,
+    borderTopWidth: 1,
+    borderColor: '#eee',
+  },
+  Input: {
+    flex: 1,
+    height: 40,
+    backgroundColor: '#eee',
+    borderRadius: 4,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#eee',
+  },
+  Button: {
+    height: 40,
+    width: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1c6cce',
+    borderRadius: 4,
+    marginLeft: 10,
+  }
 });
