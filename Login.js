c7466355dbd6a38cdb1122cbb6b8a425bde6239e
MarcosSilva13 <marcosslp45@gmail.com>
2022-05-16 17:41:54 -0300

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,105 +1,100 @@
-import { 
-    StyleSheet, 
-    Text, 
-    View, 
-    TouchableOpacity,
-    TextInput,
-    KeyboardAvoidingView,
-    Platform,
-    Alert
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
 } from "react-native";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useState } from "react";
 
 export default function Login({ navigation }) {
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-    const [errorLogin, setErrorLogin] = useState("");
-    
-    const emailTeste = "marcos@gmail.com";
-    const senhaTeste = "123456789";
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [errorLogin, setErrorLogin] = useState("");
 
-   function checkLogin() {
+  const emailTeste = "marcos@gmail.com";
+  const senhaTeste = "123456789";
 
-        if (email !== emailTeste) {
-            Alert.alert("Atenção", "Email incorreto!"); // no android
+  function checkLogin() {
+    if (email !== emailTeste) {
+      Alert.alert("Atenção", "Email incorreto!"); // no android
 
-            alert("Email incorreto!"); // na web
-        } else if(senha !== senhaTeste){
-            Alert.alert("Atenção", "Senha incorreta!"); // no android
+      alert("Email incorreto!"); // na web
+    } else if (senha !== senhaTeste) {
+      Alert.alert("Atenção", "Senha incorreta!"); // no android
 
-            alert("Senha incorreta!"); // na web
-        } else {
-            navigation.navigate("Tarefa");
-        }
+      alert("Senha incorreta!"); // na web
+    } else {
+      navigation.navigate("Tarefa");
     }
-  
-    return (
+  }
+
+  return (
     <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={style.container} 
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={style.container}
     >
-        <Text style={style.title}>Tarefas</Text>
-        <TextInput
-            style={style.input}
-            placeholder="Digite seu email"
-            type="text"
-            onChangeText={(text) => setEmail(text)}
-            value={email}
-        />
-        <TextInput
-            style={style.input}
-            secureTextEntry={true}
-            placeholder="Digite sua senha"
-            type="text"
-            onChangeText={(text) => setSenha(text)}
-            value={senha}
-        />
+      <Text style={style.title}>Tarefas</Text>
+      <TextInput
+        style={style.input}
+        placeholder="Digite seu email"
+        type="text"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+      />
+      <TextInput
+        style={style.input}
+        secureTextEntry={true}
+        placeholder="Digite sua senha"
+        type="text"
+        onChangeText={(text) => setSenha(text)}
+        value={senha}
+      />
 
-        {errorLogin === true 
-        ?
-            <View style={style.contentAlert}>
-                <MaterialCommunityIcons 
-                    name="alert-circle"
-                    size={24}
-                    color="#bdbdbd"
-                />
-                <Text style={style.warningAlert}>Email ou senha inválido!</Text>
-            </View>    
-        :
-            <View/>
-        }
+      {errorLogin === true ? (
+        <View style={style.contentAlert}>
+          <MaterialCommunityIcons
+            name="alert-circle"
+            size={24}
+            color="#bdbdbd"
+          />
+          <Text style={style.warningAlert}>Email ou senha inválido!</Text>
+        </View>
+      ) : (
+        <View />
+      )}
 
-        
-        {email === "" || senha === ""
-        ?
-        <TouchableOpacity    //botão de login
-            disabled={true}
-            style={style.buttonLoginDisable}
+      {email === "" || senha === "" ? (
+        <TouchableOpacity //botão de login
+          disabled={true}
+          style={style.buttonLoginDisable}
         >
-            <Text style={style.textButtonLogin}>Login</Text>
+          <Text style={style.textButtonLogin}>Login</Text>
         </TouchableOpacity>
-        :
+      ) : (
         <TouchableOpacity
-            style={style.buttonLogin}
-            onPress={() => checkLogin()}
-            
+          style={style.buttonLogin}
+          onPress={() => checkLogin()}
         >
-            <Text style={style.textButtonLogin}>Login</Text>
+          <Text style={style.textButtonLogin}>Login</Text>
         </TouchableOpacity>
-        }
-    
-        <Text style={style.registration}> 
-            Ainda não tem uma conta ?    
-            <Text 
-            style={style.linkSubscribe}
-            //onPress={() => navigation.navigate("Cadastro")}
-            >
-                <Text/> Cadastre-se...    
-            </Text>      
+      )}
+
+      <Text style={style.registration}>
+        Ainda não tem uma conta ?
+        <Text
+          style={style.linkSubscribe}
+          //onPress={() => navigation.navigate("Cadastro")}
+        >
+          <Text /> Cadastre-se...
         </Text>
-        <View style={{height: 100}}/>
+      </Text>
+      <View style={{ height: 100 }} />
 
       <TouchableOpacity
         style={style.Button}
@@ -108,7 +103,6 @@ export default function Login({ navigation }) {
       >
         <Text style={style.Texto}>Logar</Text>
       </TouchableOpacity>
-    
     </KeyboardAvoidingView>
   );
 }
@@ -119,14 +113,14 @@ const style = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: 50
+    paddingTop: 50,
   },
 
-  title:{
+  title: {
     fontSize: 48,
-    color: "#f92e6a",
+    color: "#D96800", //#f92e6a //#f92eca
     marginBottom: 10,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
 
   input: {
@@ -135,7 +129,7 @@ const style = StyleSheet.create({
     padding: 10,
     height: 50,
     borderBottomWidth: 1,
-    borderBottomColor: "#f92e6a",
+    borderBottomColor: "#D96800",
     marginLeft: "auto",
     marginRight: "auto",
     color: "#000",
@@ -146,7 +140,7 @@ const style = StyleSheet.create({
     height: 50,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#f92e6a",
+    backgroundColor: "#D96800",
     borderRadius: 50,
     marginTop: 30,
   },
@@ -170,7 +164,6 @@ const style = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-
   },
 
   warningAlert: {
@@ -196,7 +189,7 @@ const style = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#1c6cce",
     borderRadius: 15,
-    margin: 10
+    margin: 10,
     //marginLeft: 10
   },
 });
